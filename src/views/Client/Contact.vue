1026f01e89e25c217d79f5dcffaf5e34f311572d
Akhil Ravindran <akhilravindran3@gmail.com>
2020-11-01 13:04:57 +0530

    contact page updated

--- src/views/Client/Contact.vue
+++ src/views/Client/Contact.vue
@@ -6,31 +6,23 @@
                     <h3 class="text-uppercase">Get In Touch</h3>
                     <p>The most happiest time of the day!. Suspendisse condimentum porttitor cursus. Duis nec nulla turpis. Nulla lacinia laoreet odio, non lacinia nisl malesuada vel. Aenean malesuada fermentum bibendum.</p>
                     <div class="m-t-30">
-                        <form class="widget-contact-form" novalidate action="include/contact-form.php" role="form" method="post">
+                        <form class="widget-contact-form" novalidate role="form" method="post">
                             <div class="row">
                                 <div class="form-group col-md-6">
-                                    <label for="name">Name</label>
-                                    <input type="text" aria-required="true" name="widget-contact-form-name" required class="form-control required name" placeholder="Enter your Name">
+                                    <input type="text" id="name" aria-required="true" name="widget-contact-form-name" required class="form-control required name" placeholder="Enter your Name">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <label for="email">Email</label>
-                                    <input type="email" aria-required="true" name="widget-contact-form-email" required class="form-control required email" placeholder="Enter your Email">
+                                    <input type="email" id="email" aria-required="true" name="widget-contact-form-email" required class="form-control required email" placeholder="Enter your Email">
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="form-group col-md-12">
-                                    <label for="subject">Your Subject</label>
-                                    <input type="text" name="widget-contact-form-subject" required class="form-control required" placeholder="Subject...">
+                                    <input type="text" id="subject" name="widget-contact-form-subject" required class="form-control required" placeholder="Subject...">
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="message">Message</label>
-                                <textarea type="text" name="widget-contact-form-message" required rows="5" class="form-control required" placeholder="Enter your Message"></textarea>
+                                <textarea type="text" id="message" name="widget-contact-form-message" required rows="5" class="form-control required" placeholder="Enter your Message"></textarea>
                             </div>
-                            <!--  <div class="form-group">
-                                <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-                                <div class="g-recaptcha" data-sitekey="6LddCxAUAAAAAKOg0-U6IprqOZ7vTfiMNSyQT2-M"></div>
-                            </div>  -->
                             <button class="btn" type="submit" id="form-submit"><i class="fa fa-paper-plane"></i>&nbsp;Send message</button>
                         </form>
                     </div>
